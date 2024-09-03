3e2a84c8213112f2a2b9b643d7484caaa9f2a231
Michael Bain <150988106+Mikebain2387@users.noreply.github.com>
2024-09-03 10:42:25 -0500

    update

---
+++ app.js
@@ -0,0 +1,72 @@
+
+let currentDisplay = "0"; //current display
+let resultDisplay = false; //flag to track whether the result is displayed
+
+//! updating the display
+
+function updateDisplay(){
+ const displayElement = document.getElementById("display");
+ displayElement.textContent = currentDisplay;
+}
+
+//! setting  the display
+
+function addToDisplay(value){
+ if (currentDisplay === "0" || resultDisplay) {
+  currentDisplay = value;
+
+ }
+ else { 
+  currentDisplay += value;
+ }
+
+ resultDisplay = false;
+
+ updateDisplay();
+}
+
+
+
+
+//! calculate the result
+
+function calculateResult(){
+ try{
+  const result = eval(currentDisplay);
+  currentDisplay += "\n=" + result.toString( );
+  updateDisplay();
+ }
+ catch (error) {
+  currentDisplay += "\nError";
+  updateDisplay();
+ }
+
+ resultDisplay = true;
+}
+
+
+
+//! clearing the display
+
+function clearDisplay (){
+ currentDisplay = "0";
+ 
+
+ updateDisplay();
+}
+
+
+
+/*
+
+function clearLastElement (){
+ currentDisplay = currentDisplay.slice(0, -1);
+ if (currentDisplay === ""){
+  currentDisplay = "0";
+ }
+ updateDisplay();
+}
+ */
+
+
+
