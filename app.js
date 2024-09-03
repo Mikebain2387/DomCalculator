383066e53a15d5004e66a56e550a5d291edad916
Michael Bain <150988106+Mikebain2387@users.noreply.github.com>
2024-09-03 11:57:22 -0500

    update

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
 
+function addToDisplay(value) {
+ document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+ document.getElementById('display').value = '';
+}
+
+function calculateResult() {
+ const display = document.getElementById('display');
+ display.value = eval(display.value);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
 let currentDisplay = "0"; //current display
 let resultDisplay = false; //flag to track whether the result is displayed
 
